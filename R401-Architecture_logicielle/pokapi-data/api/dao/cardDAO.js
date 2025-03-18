f7c13e0d5cb19fd0765bbde646a3a4ec85617def
Thomas SOUCHET <thomas.souchet2@etu.univ-nantes.fr>
2025-03-18 22:32:29 +0100

    R401-data: Ajout structure DAO et tests

--- R401-Architecture_logicielle/pokapi-data/api/dao/cardDAO.js
+++ R401-Architecture_logicielle/pokapi-data/api/dao/cardDAO.js
@@ -5,7 +5,30 @@ import {Card} from "../model/Card.js";
 const cardModel = mongoose.model("Card", buildMongooseSchema(Card))
 
 const cardDAO = {
-    // TODO : Faire le DAO qui récupère les données depuis la BD de cache
+    findCardByID: async (id) => {
+        // TODO: Récupérer une carte a partir de son ID
+    },
+    findCardEvolution: async (id) => {
+        // TODO: voir le champ 'evolvesFrom'
+    },
+    findAllSets: async () => {
+        // TODO: Obtenir tous les SET avec 3 cartes qui les représentent
+    },
+    findCardsBySet: async (id) => {
+        // TODO: récupérer toutes les cartes d'un set
+    },
+    addOneCard: async (card) => {
+        // TODO: Ajoute une carte à la BD
+    },
+    addManyCards: async (cards) => {
+        // TODO: Ajoute un array de cartes à la BD
+    },
+    deleteAllCards: async () => {
+        // TODO: Vide la BD
+    },
+    updateCard: async (id, card) => {
+        // TODO: met à jour la carte (ID) avec le carte donnée
+    }
 }
 
 export default cardDAO
--- R401-Architecture_logicielle/pokapi-data/test/dao/cardDAO.test.js
+++ R401-Architecture_logicielle/pokapi-data/test/dao/cardDAO.test.js
@@ -1,1 +1,50 @@
-// TODO : Test du DAO de la BD de cache
+import mongoose from 'mongoose';
+import assert from "node:assert"
+import { describe, it, before, beforeEach, after } from "node:test"
+import cardDAO from "../../api/dao/cardDAO.js"
+
+let mongod=null
+let connexion = null
+
+describe('Dao - CardDAO', () => {
+    before(async ()=>{
+        await mongoose.connection.close()
+        const {MongoMemoryServer}  = await import('mongodb-memory-server')
+        mongod = await MongoMemoryServer.create();
+        const uri = mongod.getUri();
+        connexion = await mongoose.connect(uri)
+    })
+
+    beforeEach(async ()=>{
+        await cardDAO.deleteAllCards()
+    })
+
+    it('addOneCard', {todo: true}, () => {
+        // TODO
+    })
+
+    it('addManyCards', {todo: true}, () => {
+        // TODO
+    })
+
+    it('findCardEvolution', {todo: true}, () => {
+        // TODO
+    })
+
+    it('findAllSets', {todo: true}, () => {
+        // TODO
+    })
+
+    it('findCardsBySet', {todo: true}, () => {
+        // TODO
+    })
+
+    it('updateCard', {todo: true}, () => {
+        // TODO
+    })
+
+    after(async ()=>{
+        await mongod.stop()
+        connexion.disconnect()
+    })
+})
