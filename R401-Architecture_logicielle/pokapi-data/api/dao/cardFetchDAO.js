8f3b98eea7f6335b005f226177046ef3b1009e62
Kyllian <kyllian.arnaud@etu.univ-nantes.fr>
2025-03-18 14:51:23 +0100

    cardFecthDAO.js

--- R401-Architecture_logicielle/pokapi-data/api/dao/cardFetchDAO.js
+++ R401-Architecture_logicielle/pokapi-data/api/dao/cardFetchDAO.js
@@ -1,7 +1,29 @@
-const url = ""
+import HttpsProxyAgent from 'https-proxy-agent';
+import CONFIG from "../../const.js";
+import {Card} from "../model/Card.js";
+
+let agent = null
+if (CONFIG.PROXY != undefined) {
+    console.log(`Le proxy est ${CONFIG.PROXY}`)
+    agent =  new HttpsProxyAgent(CONFIG.PROXY);
+}
+else {
+    //pour pouvoir consulter un site avec un certificat invalide
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    console.log("Pas de proxy trouvé")
+}
+
+
+const url = "https://api.pokemontcg.io/v2/"
+const urlCard = url+"cards/"
 
 const cardFetchDAO = {
     // TODO : Faire le DAO qui récupère les données de l'API
+    findById : async (id)=> {
+        let response = await fetch(urlCard+`${id}`)
+        let json = await response.json()
+        return new Card(json.data)
+    }
 }
 
 export default cardFetchDAO
--- R401-Architecture_logicielle/pokapi-data/const.js
+++ R401-Architecture_logicielle/pokapi-data/const.js
@@ -9,7 +9,8 @@ const CONFIG = {
     ENV: process.env.ENV || 'PROD',
     MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017',
     MONGO_DB: process.env.MONGO_DB || 'pokapiDataDB',
-    API_POKEMON_KEY: process.env.API_POKEMON_KEY || undefined
+    API_POKEMON_KEY: process.env.API_POKEMON_KEY || undefined,
+    PROXY: process.env.PROXY || undefined
 }
 
 export default CONFIG
