3ef756ccef85964be6f24d78b1757432216457c2
Thomas SOUCHET <thomas.souchet2@etu.univ-nantes.fr>
2025-03-29 16:26:16 +0100

    R401-stub: Ajout des routes manquantes + modif

--- R401-Architecture_logicielle/pokapi-stub/route.js
+++ R401-Architecture_logicielle/pokapi-stub/route.js
@@ -4,6 +4,7 @@ import express from 'express'
 
 import boosterContent from './assets/booster.json' with {type: 'json'}
 import rarestCards from './assets/rarest.json' with {type: 'json'}
+import sets from './assets/sets.json' with {type: 'json'}
 
 const router = express.Router()
 
@@ -11,76 +12,123 @@ const router = express.Router()
 // Data
 // ---
 
-router.route("/openBooster/:SETID").get((req,res)=>{
-    res.send(boosterContent)
-})
-
-router.route("/getRarestCard").get((req,res)=>{
-    res.status(200).send(rarestCards)
-})
-
-router.route("/getMostSearchedCard").get((req,res)=>{
-    res.send(rarestCards)
+router.route("/open-booster/:SETID").get((req,res)=>{
+    res.status(200).send(boosterContent)
 })
 
 router.route("/card/:ID").get((req,res)=>{
-    res.send(rarestCards[0])
+    res.status(200).send(rarestCards[0])
+})
+
+router.route("/cards").post((req,res) => {
+    res.status(200).send(rarestCards)
+})
+
+router.route("/card/evolution/:ID").get((req,res) => {
+    res.status(200).send(rarestCards[0])
+})
+
+router.route("/set/presentation/:SETID").get((req,res) => {
+    const finalObject = {}
+    finalObject.set = sets.data[0]
+    finalObject.images = []
+    for (let i=0; i<4; i++) {
+        finalObject.images.push(boosterContent[i].images.large)
+    }
+    res.status(200).send(finalObject)
+})
+
+router.route("/set/:SETID").get((req,res) => {
+    res.status(200).send(rarestCards)
+})
+
+router.route("/sets").get((req,res) => {
+    res.status(200).send(sets)
+})
+
+router.route("/deck-price").post((req,res) => {
+    res.status(200).send(53.2)
 })
 
 // ---
 // User
 // ---
-
-router.route("/getBestUsersCollections").get((req,res)=>{
-    res.send([
-        {
-            pseudo : "Kiki",
-            collection : rarestCards
-        },
-        {
-            pseudo : "Toto",
-            collection : boosterContent
-        }
-    ])
-})
+const stubToken = "TGVzQ2hhdXNzZXR0ZXNEZUwnYXJjaGlEdWNoZXNzZVNvbnRFbGxlc1PDqGNoZXM="
+const stubLogin = "admin"
+const stubPassword = "admin"
 
 router.route("/login").post((req,res)=>{
-    if(req.body.login === "admin" && req.body.password === "thomas"){
+    if (req.body.login === stubLogin && req.body.password === stubPassword) {
         res.status(200).send({
-            token : "LesChaussettesDeL'archiDuchesseSontEllesSèches"
+            token : stubToken
         })
-    }else{
+    } else {
         res.status(401).send()
     }
 })
 
 router.route("/register").post((req,res)=>{
-    if(req.body.login === "admin" && req.body.password === "thomas"){
+    if(req.body.login === stubLogin && req.body.password === stubPassword){
         res.status(401).send()
-    }else{
+    } else {
         res.status(200).send({
-            token : "LesChaussettesDeL'archiDuchesseSontEllesSèches"
+            token : stubToken
         })
     }
 })
 
-router.route("/getInfo").get((req,res)=>{
-    console.log(req.headers)
-    if(req.headers["authentification-token"] != "LesChaussettesDeL'archiDuchesseSontEllesSèches"){
+router.route("/my-cards").get((req,res)=>{
+    if(req.headers["authentification-token"] !== stubToken){
+        res.status(401).send()
+        return
+    }
+    res.status(200).send(rarestCards)
+})
+
+router.route("/user/:PSEUDO").get((req,res)=>{
+    res.status(200).send({
+        login: stubLogin,
+        pseudo: req.params.PSEUDO
+    })
+})
+
+router.route("/searched/add").post((req,res)=>{
+    if(req.headers["authentification-token"] !== stubToken){
+        res.status(401).send()
+        return
+    }
+    res.status(200).send()
+})
+
+router.route("/searched").get((req,res)=>{
+    if(req.headers["authentification-token"] !== stubToken){
+        res.status(401).send()
+        return
+    }
+    res.status(200).send(boosterContent)
+})
+
+router.route("/info").get((req,res)=>{
+    if(req.headers["authentification-token"] !== stubToken){
         res.status(401).send()
         return
     }
     res.status(200).send({
+        login: stubLogin,
         pseudo : "Thomas"
     })
 })
 
 router.route("/update").put((req,res)=>{
-    if(req.headers["authentification-token"] != "LesChaussettesDeL'archiDuchesseSontEllesSèches"){
+    if(req.headers["authentification-token"] !== stubToken){
         res.status(401).send()
         return
     }
     res.status(200).send()
+})
+
+router.route("/searched/popular").get((req,res)=>{
+    res.status(200).send(rarestCards)
 })
 
 export default router
