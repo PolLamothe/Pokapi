0e4a96c3351ffef3bfeb2467e0cbaef5848898b0
Kyllian <kyllian.arnaud@etu.univ-nantes.fr>
2025-03-30 16:54:29 +0200

    R401-ajout Rarites.json + modif types.json + route.js / R410-update page Collection

--- R410-Complement_web/pokapi/src/pages/Collection.jsx
+++ R410-Complement_web/pokapi/src/pages/Collection.jsx
@@ -1,15 +1,15 @@
 import config from "../config.js";
 import {useEffect, useState} from "react";
-import {Box, CheckboxGroup, Flex, Grid, TextField} from "@radix-ui/themes";
+import {Box, CheckboxGroup, Flex, Grid, ScrollArea, TextField} from "@radix-ui/themes";
 import {MagnifyingGlassIcon} from "@radix-ui/react-icons";
 import * as Accordion from '@radix-ui/react-accordion';
 import {AccordionContent, AccordionTrigger} from "@radix-ui/react-accordion";
-import {ChevronDownIcon} from "lucide-react";
+import {ChevronDownIcon, Scroll} from "lucide-react";
 
 
 function ImageCard({card}) {
     return <Flex justify="center">
-        <img className="rt-r-px-2 rt-r-py-2" alt={card.name} src={card.images.small}/>
+        <img className="rt-r-px-2 rt-r-py-2" alt={card.name} src={card.images.small} style={{ maxWidth: "261px" , maxHeight: "358px" }} />
     </Flex>
 }
 
@@ -17,6 +17,8 @@ function Collection() {
 
     const [userCards, setUserCards] = useState([])
     const [types, setTypes] = useState([])
+    const [rarities, setRarities] = useState([])
+    const [sets, setSets] = useState([])
 
 
     useEffect(()=>{
@@ -42,14 +44,35 @@ function Collection() {
             setTypes(dataTypes)
         }
         fetchAllTypes()
-    }, [setTypes]);
+    }, [setTypes])
 
-    console.log(userCards)
+    useEffect(() => {
+        const fetchRarities = async () => {
+            let allRarities = await fetch(config.url + "/rarities", {
+                method: "GET"
+            })
+            let dataRarities = await allRarities.json()
+            setRarities(dataRarities)
+        }
+        fetchRarities()
+    }, [setRarities])
+
+    useEffect(() => {
+        const fetchSets = async () => {
+            let allSets = await fetch(config.url + "/sets", {
+                method: "GET"
+            })
+            let dataSets = await allSets.json()
+            setSets(dataSets)
+        }
+        fetchSets()
+    }, [setSets])
+
 
     return (
         <Grid columns="3" style={{gridTemplateColumns:'25% 75%'}}>
             <Flex id="searchBar" px="5" py="5" justify="center" style={{gridColumn: 'span 3'}} >
-                <TextField.Root radius="full" placeholder="Search Pokemon ..." size="3" style={{width:'80vh'}} >
+                <TextField.Root radius="full" placeholder="Rechercher un Pokemon ..." size="3" style={{width:'80vh'}} >
                     <TextField.Slot>
                         <MagnifyingGlassIcon height="16" width="16" />
                     </TextField.Slot>
@@ -64,9 +87,13 @@ function Collection() {
                         <AccordionContent className="AccordionContent">
                             <CheckboxGroup.Root defaultValue='All' name="type">
                                 <CheckboxGroup.Item value="All">All</CheckboxGroup.Item>
-                                {types.map((type) => (
-                                    <CheckboxGroup.Item key={type} value={type}>{type}</CheckboxGroup.Item>
-                                ))}
+                                {types && types.data ? (
+                                    types.data.map((type) => (
+                                        <CheckboxGroup.Item key={type} value={type}>{type}</CheckboxGroup.Item>
+                                    ))
+                                ) : (
+                                    <p>Aucun Type trouvée.</p>
+                                )}
                             </CheckboxGroup.Root>
                         </AccordionContent>
                     </Accordion.Item>
@@ -74,20 +101,42 @@ function Collection() {
                     <Accordion.Item value="Rareté" className="AccordionItem">
                         <AccordionTrigger className="AccordionTrigger">Rareté <ChevronDownIcon className="AccordionChevron" aria-hidden /> </AccordionTrigger>
                         <AccordionContent className="AccordionContent">
-                            TEMP
+                            <ScrollArea className="raritiesScroll" style={{height: '250px'}}>
+                                <CheckboxGroup.Root defaultValue='All' name="rarities">
+                                    <CheckboxGroup.Item value="All">All</CheckboxGroup.Item>
+                                    {rarities && rarities.data ? (
+                                        rarities.data.map((rarity) => (
+                                        <CheckboxGroup.Item key={rarity} value={rarity}>{rarity}</CheckboxGroup.Item>
+                                        ))
+                                    ): (
+                                        <p>Aucune Rareté trouvé !</p>
+                                    )}
+                                </CheckboxGroup.Root>
+                            </ScrollArea>
                         </AccordionContent>
                     </Accordion.Item>
 
                     <Accordion.Item value="Set" className="AccordionItem">
                         <AccordionTrigger className="AccordionTrigger">Set <ChevronDownIcon className="AccordionChevron" aria-hidden /> </AccordionTrigger>
                         <AccordionContent className="AccordionContent">
-                            TEMP
+                            <ScrollArea className="raritiesScroll" style={{height: '250px'}}>
+                                <CheckboxGroup.Root defaultValue='All' name="sets">
+                                    <CheckboxGroup.Item value="All">All</CheckboxGroup.Item>
+                                    {sets && sets.data ? (
+                                        sets.data.map(set => (
+                                        <CheckboxGroup.Item key={set.id} value={set.id}>{set.name}</CheckboxGroup.Item>
+                                        ))
+                                    ) : (
+                                        <p>Aucun Sets trouvée !</p>
+                                    )}
+                                </CheckboxGroup.Root>
+                            </ScrollArea>
                         </AccordionContent>
                     </Accordion.Item>
 
                 </Accordion.Root>
             </Flex>
-            <Grid className="cardsUser" columns="repeat(auto-fit, minmax(261px, 1fr))" style={{maxWidth: '1500px'}}>
+            <Grid className="cardsUser" columns="repeat(auto-fit, minmax(261px, 1fr))" style={{maxWidth: '1500px', height: "fit-content"}}>
                 {userCards.map(card => (
                     <ImageCard key={card.name} card={card}/>
                 ))}
