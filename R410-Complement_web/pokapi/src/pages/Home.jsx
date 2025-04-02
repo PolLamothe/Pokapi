cd07894e0582365f5a1edbf659247bcff78a0a65
Pol Lamothe <lamothepol@gmail.com>
2025-04-02 17:13:35 +0200

    Carousel avec fleche de naviguation

--- R410-Complement_web/pokapi/src/pages/Home.jsx
+++ R410-Complement_web/pokapi/src/pages/Home.jsx
@@ -1,37 +1,13 @@
 import SetPresentation from "../components/SetPresentation";
 import dao from "../dao/pokapiDAO.js"
 import React,{useState,useEffect} from "react"
+import Carousel from "../components/Carrousel.jsx";
 
 function Home() {
-    const [listSet,setListSet] = useState(null)
-
-    const [loadedSet,setLoadedSet] = useState([])
-
-    useEffect(()=>{
-        async function retrieveSetList(){
-            let newListSet = (await dao.fetchSets()).data
-            let newLoadedSet = []
-            for(let i = 0;i<3;i++){
-                const index = parseInt(Math.random()*newListSet.length-1)
-                newLoadedSet.push(newListSet[index])
-                newListSet.splice(index,1)
-            }
-            setListSet(newListSet)
-            setLoadedSet(newLoadedSet)
-        }
-        retrieveSetList()
-    },[setListSet])
-
     return (
         <>
             <h3>Page d'accueil (voir les booster)</h3>
-            <div style={setWrapperStyle}>
-                {
-                    loadedSet && loadedSet.map((set,index)=>
-                        <SetPresentation setId={set.id}></SetPresentation>
-                    )
-                }    
-            </div>
+            <Carousel></Carousel>
         </>
     )
 }
