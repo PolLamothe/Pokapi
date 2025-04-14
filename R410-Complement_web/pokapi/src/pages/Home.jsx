8315934564198f16823fb4efd9dcf06e42faa944
Pol Lamothe <lamothepol@gmail.com>
2025-04-14 09:25:11 +0200

    fixing

--- R410-Complement_web/pokapi/src/pages/Home.jsx
+++ R410-Complement_web/pokapi/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from "react"
 import Carousel from "../components/Carrousel.jsx";
 import {Button} from "@radix-ui/themes";
+import BoosterOpening from "../components/boosterOpening.jsx";
 
 function Home() {
 
