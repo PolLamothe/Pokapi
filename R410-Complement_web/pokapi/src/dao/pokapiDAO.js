5fec1d2e968bfc13b67583e5425968564fef1de9
Pol Lamothe <lamothepol@gmail.com>
2025-04-15 21:27:48 +0200

    Signalisation des cartes recherchées dans collection

--- R410-Complement_web/pokapi/src/dao/pokapiDAO.js
+++ R410-Complement_web/pokapi/src/dao/pokapiDAO.js
@@ -186,6 +186,18 @@ const pokapiDAO = {
         }
         throw new Error((await response.json()).message)
     },
+    fetchSearched : async ()=>{
+        const response = await fetch(config.url+"/searched",{
+            method:"GET",
+            headers: {
+                ...baseHeaders,
+                "Authentification-Token": localStorage.getItem("token")
+            },
+        })
+        if(response.status == 200){
+            return await response.json()
+        }
+    }
 }
 
 export default pokapiDAO
